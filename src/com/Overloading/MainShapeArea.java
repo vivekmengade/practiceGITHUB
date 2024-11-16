27f7bce59b6502dde2f42ab78bb02760e5e712df
vivekmengade <vivekmengade@gmail.com>
2024-11-16 17:19:08 +0530

    Example of area calculating using overloading

---
+++ src/com/Overloading/MainShapeArea.java
@@ -0,0 +1,11 @@
+package com.Overloading;
+
+public class MainShapeArea {
+	public static void main(String[] args) {
+		ShapeArea a = new ShapeArea();
+		a.area(5);
+		a.area(5, 10);
+		a.area(5,10, "Triangle");
+		a.area(5,"circle");
+	}
+}
