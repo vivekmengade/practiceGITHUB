2f01786c9dd06e653f0beed8b66323ab3698a322
vivekmengade <vivekmengade@gmail.com>
2024-12-05 18:20:17 +0530

    added

--- src/com/FileHandling/FileTest.java
+++ src/com/FileHandling/FileTest.java
@@ -1,13 +1,16 @@
 package com.FileHandling;
 
-import java.io.File;
-import java.io.IOException;
+
+import java.io.*;
 
 public class FileTest {
 	
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
 		System.out.println("hello");
+		
+		/*
+		//create file
 		File file = new File("D:\\New folder\\hello.txt");
 		try {
 			if(file.createNewFile()) {
@@ -19,5 +22,59 @@ public class FileTest {
 			e.printStackTrace();
 		}
 		
+		
+		//write file
+		FileWriter Write = new FileWriter(file);
+		Write.write("Good Morning Everyone....");
+		Write.write("Hello, Nice To Meet You....");
+		Write.close();
+		
+		
+		//read file
+		FileReader Read = new FileReader(file);
+		int num=0;
+		while((num=Read.read())!=-1) {
+			System.out.print((char)num);
+		}
+		
+		
+		//delete file
+		if(file.delete()) {
+			System.out.println("File Deleted");
+			
+		}else {
+			System.out.println("Somethimh went wrong");
+		}
+		*/
+		
+		
+		File file = new File("D:\\New folder\\New file.txt");
+		 if(file.createNewFile()) {
+			 System.out.println("File is Created");
+		 }else {
+			 System.out.println("Problem in Creation");
+		 }
+		 
+		 
+		 FileWriter Write = new FileWriter(file);
+		 Write.write("Hello... ");
+		 Write.write("Writing File in path.....");
+		 Write.close();
+		 
+		 FileReader Read = new FileReader(file);
+		 int num=0;
+		 while((num=Read.read())!=-1) {
+			 System.out.print((char)num);
+		 }
+		 
+		 
+		 
+		 
+		 /*
+		 if(file.delete()) {
+			 System.out.println("File is Deleted");
+		 }else {
+			 System.out.println("Problem in deleting....");
+		 }*/
 	}
 }
---
+++ src/com/FileHandling/Hello.java
@@ -0,0 +1,8 @@
+package com.FileHandling;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
