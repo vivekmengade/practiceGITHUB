ce9fd121c84320c928a3bcc56b291b55522cbb71
vivekmengade <vivekmengade@gmail.com>
2024-12-11 18:49:27 +0530

    simple practice

--- fileHandling/com/Map/Student.java
+++ fileHandling/com/Map/Student.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class Student {
 	//practice about map hashmap,  linkedhashmap, tree map
@@ -56,6 +57,30 @@ public class Student {
 		System.out.println(m2.values());
 		System.out.println("---------------");
 		Collection<String> values3= m2.values();
+		System.out.println(values);
+		
+		
+		Map<Integer, String> m3 = new TreeMap<>();
+		m2.put(001, "Vivek");
+		m2.put(002, "Nihar");
+		m2.put(003, "Amruta");
+		System.out.println(m3);
+		Set s5= m3.keySet();
+		Iterator itr5 = s3.iterator();
+		while(itr1.hasNext()) {
+			System.out.println(itr1.next());
+		}
+		System.out.println("---------------");
+		Set s6 = m3.entrySet();
+		Iterator itr4 = s2.iterator();
+		while(itr2.hasNext()) {
+			System.out.println(itr2.next());
+		}
+		
+		System.out.println("---------------");
+		System.out.println(m3.values());
+		System.out.println("---------------");
+		Collection<String> values4= m3.values();
 		System.out.println(values);
 	}
 }
