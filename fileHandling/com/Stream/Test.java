9f8ef114a19c80e370d805c6bbc2f08af29b91b7
vivekmengade <vivekmengade@gmail.com>
2024-12-19 19:46:01 +0530

    Learned about map Method

--- fileHandling/com/Stream/Test.java
+++ fileHandling/com/Stream/Test.java
@@ -7,10 +7,17 @@ import java.util.stream.Stream;
 
 public class Test {
 	public static void main(String[] args) {
-		List<Integer> l1 = Arrays.asList(11,12,13,14,15,16,17,18,19,20);
+		List<Integer> l1 = Arrays.asList(14,12,15,11,16,17,19,18,20);
 		Stream<Integer> s1 = l1.stream();
-		s1.filter(i-> i%2==0).collect(Collectors.toList());
-		System.out.println(l1);
 		
+//		Filter Method
+		List<Integer> list = s1.filter(i-> i%2==0).collect(Collectors.toList());
+		System.out.println("These elements are all even Elements"+list);
+		
+		
+//		Map Method
+		Stream<Integer> s2=l1.stream();
+		List<Integer> Square= s2.map(ele -> ele*ele).collect(Collectors.toList());
+		System.out.println("Square of all elemets"+Square);
 	}
 }
